660ccdd32fd549342774941b6bc999d7c1c00c74
theminhdayma <147359915+theminhdayma@users.noreply.github.com>
2025-01-16 00:30:41 +0700

    Add session02

--- session02/Bai3/bai3.sql
+++ session02/Bai3/bai3.sql
@@ -3,7 +3,13 @@ use session02;
 
 create table Product (
 	id_product int key, 
-    name_product varchar(100) not null,
-    price decimal not null,
+    name_product varchar(100),
+    price decimal,
     quantity int
 );
+
+alter table Product
+modify name_product varchar(255) not null;
+
+alter table Product
+modify price decimal(10, 2) not null;
