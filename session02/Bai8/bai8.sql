e3a06810a8743ca7a9b8453c7be101ca679edc05
theminhdayma <147359915+theminhdayma@users.noreply.github.com>
2025-01-16 01:18:46 +0700

    Add session02

--- session02/Bai8/bai8.sql
+++ session02/Bai8/bai8.sql
@@ -3,27 +3,47 @@ use session02;
 
 /*
 a) Phân tích và liệt kê các vấn đề trong bảng
-1. Thiếu khóa chính (Primary Key):
- - Cột maSV hiện tại không được xác định là khóa chính
- - Thiếu ràng buộc NOT NULL:
-
-Cột maSV và diem có thể chứa giá trị NULL
-Thiếu kiểm tra giá trị điểm (Diem):
-
-Cột diem không có ràng buộc kiểm tra (CHECK constraint)
+- Thiếu khóa chính cho bảng điểm
+- Bảng điểm là thực thể yếu nên thiếu ít nhất 2 khóa chính
+ví dụ là mã sinh viên và mã môn
+	
 */
+create table Student (
+	idStu varchar(20) primary key,
+    nameStu varchar(255) not null,
+    ageStu int
+);
+
+insert into Student (idStu, nameStu, ageStu)
+values
+('SV001', 'Nguyen Van A', 20),
+('SV002', 'Le Thi B', 21),
+('SV003', 'Tran Van C', 19);
+
+create table Subject (
+	idSub varchar(20) primary key,
+    nameSub varchar(100) not null
+);
+
+insert into Subject (idSub, nameSub)
+values
+('SUB001', 'Toán'),
+('SUB002', 'Vật lý'),
+('SUB003', 'Hóa học');
 
 create table Diem(
-    maSV varchar(20) not null,
+    idStu varchar(20) not null,
+    idSub varchar(20) not null,
     diem int not null,
-    primary key (maSV),
+    primary key (idStu, idSub),
+    foreign key (idStu) references Student(idStu),
+    foreign key (idSub) references Subject(idSub),
     check (diem >= 0 and diem <= 10)
 );
 
-insert into Diem (maSV, diem)
-VALUES
-('SV001', 8),
-('SV002', 9),
-('SV003', 7),
-('SV004', 10),
-('SV005', 5);
+insert into Diem (idStu, idSub, diem)
+values
+('SV001', 'SUB001', 8),
+('SV002', 'SUB002', 9),
+('SV003', 'SUB003', 7);
+
